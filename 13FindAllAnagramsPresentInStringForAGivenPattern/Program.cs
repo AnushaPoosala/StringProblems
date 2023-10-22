0f1cadb01ef11409e11e6323b442627b15662f01
AnushaPoosala <117409595+AnushaPoosala@users.noreply.github.com>
2023-10-22 16:26:13 -0400

    Count of anagrams with different input check

--- 13FindAllAnagramsPresentInStringForAGivenPattern/Program.cs
+++ 13FindAllAnagramsPresentInStringForAGivenPattern/Program.cs
@@ -56,7 +56,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Program program = new Program();
-            program.FindAllAnagramsInStringForAGivenPattern("abcd", "bcdcbabcbd");
+            program.FindAllAnagramsInStringForAGivenPattern("abcd", "bcdcbabcbabcd");
         }
     }
 }
